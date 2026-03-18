45939fc196d8a89c99962f015a8fe057a6836665
Patryk Sitko <patryk.sitko.algemeen@gmail.com>
2026-03-18 10:55:49 +0100

    Bugfixes.

--- server/app.js
+++ server/app.js
@@ -1,19 +1,33 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors";
+import helmet from "helmet";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
+app.use(helmet());
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static("../client/build"));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+// React Router fallback for Single Page Applications
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
 export default app;
